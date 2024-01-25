1b27739e21d81b23b32bb303f3cd73d7e0dddcac
mrGolbya <mr.golbya@gmail.com>
2024-01-25 16:10:42 +0300

    feat: add button

---
+++ menu-burger.js
@@ -0,0 +1,7 @@
+const NAV = document.querySelector(".line-2");
+const MOBILE_MENU = document.querySelector(".mobile-menu-toggler")
+
+MOBILE_MENU.addEventListener("click", () => {
+  MOBILE_MENU.classList.toggle("active");
+  NAV.classList.toggle("active");
+});
